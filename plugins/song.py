9f86dd7519fb4bde8963f5685db494aaa670fd2e
Disney team <83995690+disneyteam75@users.noreply.github.com>
2021-05-20 16:06:44 +0530

    Update

--- plugins/song.py
+++ plugins/song.py
@@ -85,7 +85,7 @@ def a(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🏷 <b>Title:</b> <a href="{link}">{title}</a>\n⏳ <b>Duration:</b> <code>{duration}</code>\n👀 <b>Views:</b> <code>{views}</code>\n🎧 <b>Uploaded By:</b> <a href="https://t.me/safonemusicbot">𝚂𝚊𝚏𝚘𝚗𝚎 𝙼𝚞𝚜𝚒𝚌</a>'
+        rep = f'🏷 <b>Title:</b> <a href="{link}">{title}</a>\n⏳ <b>Duration:</b> <code>{duration}</code>\n👀 <b>Views:</b> <code>{views}</code>\n🎧 <b>Uploaded By:</b> <a href="https://t.me/disneygrou">Disney group</a>'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
