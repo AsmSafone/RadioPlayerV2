14d43c0acab0d4fa520b31ef142d5f1b2bde8588
SAF ONE <77989182+AsmSafone@users.noreply.github.com>
2021-06-29 13:34:05 +0600

    Hope Fixed 😅

--- config.py
+++ config.py
@@ -21,16 +21,16 @@ import re
 
 
 class Config:
-    ADMIN = os.environ.get("ADMINS", '')
+    ADMIN = os.environ.get("ADMINS", "1316963576")
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
     API_ID = int(os.environ.get("API_ID", 12345))
     CHAT = int(os.environ.get("CHAT", ""))
-    LOG_GROUP=os.environ.get("LOG_GROUP", "")
+    LOG_GROUP = os.environ.get("LOG_GROUP", "")
     if LOG_GROUP:
-        LOG_GROUP=int(LOG_GROUP)
+        LOG_GROUP = int(LOG_GROUP)
     else:
-        LOG_GROUP=None
-    STREAM_URL=os.environ.get("STREAM_URL", "https://radioindia.net/radio/hungamanow/icecast.audio")
+        LOG_GROUP = None
+    STREAM_URL = os.environ.get("STREAM_URL", "https://radioindia.net/radio/hungamanow/icecast.audio")
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     SESSION = os.environ.get("SESSION_STRING", "")
