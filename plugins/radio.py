69ed6b5c25d16c6bf9a6fa74f3f07cb0a611ea78
Sobuj Khan <86087549+SobujKhanSK@users.noreply.github.com>
2021-07-09 01:20:27 +0600

    Update radio.py

--- plugins/radio.py
+++ plugins/radio.py
@@ -1,6 +1,6 @@
 """
 RadioPlayerV2, Telegram Voice Chat Userbot
-Copyright (C) 2021  Asm Safone
+Copyright (C) 2021  Sobuj Khan
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
