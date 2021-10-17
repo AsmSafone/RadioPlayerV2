936823d9cd6104b05bde1eb34319a3beef657ce6
Darshan <92586012+XDacchuX@users.noreply.github.com>
2021-10-17 19:34:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 RadioPlayerV2, Telegram Voice Chat Bot
-Copyright (C) 2021  Asm Safone <https://t.me/AsmSafone>
+Copyright (C) 2021  Dacchu <https://t.me/XDacchuX>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
@@ -48,7 +48,7 @@ bot.run(main())
 bot.start()
 @bot.on_message(filters.command("restart") & filters.user(Config.ADMINS))
 def restart(client, message):
-    message.reply_text("🔄 **Restarting... Join @AsmSafone!**")
+    message.reply_text("🔄 **Restarting... Join @DacchuLive**")
     Thread(
         target=stop_and_restart
         ).start()
